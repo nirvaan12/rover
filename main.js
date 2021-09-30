f992c3f4e21adcfe249a0c10bd0913821567701c
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2021-09-30 19:53:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ ctx =canvas.getContext("2d");
 
 rover_width=100;
 rover_height=90;
-
+nasa_array=["image1.jpg","image2.jpg","image3.jpg","image4.jpg"];
+randomnumber= Math.floor(Math.random()*4);
 rover_x =10;
 rover_y =10;
 
- background_img = "mars.jpg";
+ background_img = nasa_array[randomnumber];
  rover_image ="rover.png";
 
 function add(){
@@ -54,3 +55,37 @@ function myKeyDown(e){
         console.log("right");
     }
 }
+
+function up(){
+    if(rover_y>=0){
+        rover_y=rover_y-10;
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+function down(){
+    if(rover_y<=400){
+        rover_y=rover_y+10;
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+function left(){
+    if(rover_x>=0){
+        rover_x=rover_x-10;
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+function right(){
+    if(rover_x<=700){
+        rover_x=rover_x+10;
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+
